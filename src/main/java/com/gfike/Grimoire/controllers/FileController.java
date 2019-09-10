cafab1a16a21ecf9c5994f8da89be363cee0d0d7
gfike19@gmail.com <Vaughn*573>
2019-09-10 11:55:42 -0500

    got code for uploading excel, need to add dependencies

---
+++ src/main/java/com/gfike/Grimoire/controllers/FileController.java
@@ -0,0 +1,35 @@
+package com.gfike.Grimoire.controllers;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.aspectj.weaver.ast.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class FileController {
+    @PostMapping("/import")
+    public void mapReapExcelDatatoDB(@RequestParam("file") MultipartFile reapExcelDataFile) throws IOException {
+
+        List<Test> tempStudentList = new ArrayList<Test>();
+        XSSFWorkbook workbook = new XSSFWorkbook(reapExcelDataFile.getInputStream());
+        XSSFSheet worksheet = workbook.getSheetAt(0);
+
+        for(int i=1;i<worksheet.getPhysicalNumberOfRows() ;i++) {
+            Test tempStudent = new Test();
+
+            XSSFRow row = worksheet.getRow(i);
+
+            tempStudent.setId((int) row.getCell(0).getNumericCellValue());
+            tempStudent.setContent(row.getCell(1).getStringCellValue());
+            tempStudentList.add(tempStudent);
+        }
+    }
+}
